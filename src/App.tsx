29234aafb23f563aabfd724e06914be621353687
VitaliyGuzun <vitaliy@miro.com>
2024-06-24 20:32:10 +0200

    Remove title

--- src/App.tsx
+++ src/App.tsx
@@ -66,13 +66,6 @@ export default function Example() {
       }}
     >
       <div className="px-4 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:items-center">
-          <div className="sm:flex-auto">
-            <h1 className="text-base font-semibold leading-6 text-gray-900">
-              Предварительный расчет
-            </h1>
-          </div>
-        </div>
         <div className="mt-8 flow-root">
           <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle">
